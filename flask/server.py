3f031dcfcad5e65643541c0f24442d9452d8f8cc
Erika Kettleson <erikakettleson@Erikas-MacBook-Air.local>
2017-03-12 18:06:26 -0700

    adding jinja, removing ds store

---
+++ flask/server.py
@@ -0,0 +1,70 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+
+app = Flask(__name__)
+
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+# YOUR ROUTES GO HERE
+
+
+@app.route('/')
+def show_homepage():
+    """show homepage template"""
+    return render_template("index.html")
+
+
+@app.route('/application-form.html')
+def apply():
+    """show the application page"""
+    return render_template("application-form.html")
+
+
+@app.route('/application-success.html', methods=['POST', 'GET'])
+def success():
+    """show the response/success page"""
+    if request.method == 'POST':
+        # import ipdb; ipdb.set_trace()
+        name = request.form["firstname"]
+        last = request.form["lastname"]
+        money = request.form["salary"]
+        position = request.form["jobtype"]
+        return render_template("application-success.html",
+                               first=name,
+                               surname=last,
+                               salary=float(money),
+                               job=position,
+                               )
+        # else:
+        #     return render_template('application-form.html')
+
+# @app.route('/greet')
+# def greet_person():
+#     """Greet person, passing compliments if they want them."""
+
+#     player = request.args.get("person")
+#     wants_compliments = request.args.get("wants_compliments")
+
+#     if wants_compliments:
+#         nice_things = sample(COMPLIMENTS, 3)
+
+#     else:
+#         nice_things = []
+
+#     return render_template("compliments.html",
+#                            compliments=nice_things,
+#                            name=player)
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="localhost")
